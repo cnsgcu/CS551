46c58ffcc2ddcdafa78a528266efd72fae83c57c
Cuong Cu <csc823@gmail.com>
2015-09-17 20:00:09 -0500

    Lab 3

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@
     chatApp.config(function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl : 'pages/login.html',
+                templateUrl : 'pages/login.html'
             })
 
             .when('/home', {
@@ -22,60 +22,58 @@
 
     chatApp.controller('homeController', function($scope, $http) {
 
-    var mapCanvas = document.getElementById('map');
-    var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(39.09972, -94.57833),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-
-
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-
-    var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-        draggable: true
-    });
-
-    // Route
-    $scope.getRoute = function() {
-        var request = {
-            origin: $scope.route.origin,
-            destination: $scope.route.destination,
-            travelMode: google.maps.TravelMode.DRIVING
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(39.09972, -94.57833),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         };
 
-        directionsService.route(request, function(response, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                directionsDisplay.setMap(map);
-                directionsDisplay.setDirections(response);
-            }
+
+
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        var directionsService = new google.maps.DirectionsService();
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+            draggable: true
         });
-    };
 
-    $http.get('http://api.wunderground.com/api/36b799dc821d5836/conditions/q/MO/Kansas%20City.json')
-        .success(function(data) {
-            var temp = data.current_observation.temp_f;
-            var icon = data.current_observation.icon_url;
-            var weather = data.current_observation.weather;
+        // Route
+        $scope.getRoute = function() {
+            var request = {
+                origin: $scope.route.origin,
+                destination: $scope.route.destination,
+                travelMode: google.maps.TravelMode.DRIVING
+            };
 
-            var contentString = "<img src='" + icon  +"'/>" + weather + '<span> - ' + temp + ' &deg; F' + '</span>';
-
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
+            directionsService.route(request, function(response, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setMap(map);
+                    directionsDisplay.setDirections(response);
+                }
             });
 
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(39.09972, -94.57833),
-                map: map
-            });
+            $scope.weatherInfo();
+        };
 
-            marker.addListener('click', function() {
-                infowindow.open(map, marker);
-            });
+        $scope.weatherInfo = function() {
+            var orgLocation = $scope.route.origin.split(',');
+            var destLocation = $scope.route.destination.split(',');
 
-        });
+            $http.get('http://api.wunderground.com/api/36b799dc821d5836/conditions/q/' + destLocation[1].trim() + '/' + destLocation[0].trim() + '.json')
+                .success(function(data) {
+                    $scope.destination_weather_icon = data.current_observation.icon_url;
+                    $scope.destination_weather_condition = data.current_observation.weather;
+                    $scope.destination_weather_temp = data.current_observation.temp_f;
+                });
+
+            $http.get('http://api.wunderground.com/api/36b799dc821d5836/conditions/q/' + orgLocation[1].trim() + '/' + orgLocation[0].trim() + '.json')
+                .success(function(data) {
+                    $scope.origin_weather_icon = data.current_observation.icon_url;
+                    $scope.origin_weather_condition = data.current_observation.weather;
+                    $scope.origin_weather_temp = data.current_observation.temp_f;
+                });
+        }
     });
 
     chatApp.controller('registerController', function($scope) {
