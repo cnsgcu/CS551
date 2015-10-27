ea8d6316ae5f17411d1b7ffe535ca7a6da608ad0
Cuong Cu <csc823@gmail.com>
2015-10-27 01:54:57 -0500

    Lab 8

--- Source/lab 8/src/cs5551/lab8/UserServlet.java
+++ Source/lab 8/src/cs5551/lab8/UserServlet.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
